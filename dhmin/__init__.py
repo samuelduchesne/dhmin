744285314b6cc07d67600f9a95dc844f3ea4ab7d
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2019-01-19 03:51:37 -0500

    Moved dhmintools to package

--- dhmin/__init__.py
+++ dhmin/__init__.py
@@ -1,1 +1,2 @@
 from .dhmin import *
+from .dhmintools import *
--- dhmin/dhmintools.py
+++ dhmin/dhmintools.py
@@ -11,17 +11,17 @@ import dhmin
 
 def plot_flows_min(model):
     """ Plot power flows for minimal example.
-    
+
     Creates a minimal visualisation of the `mnl.xlsx` model provided with
     DHMIN.
     """
-    
+
     power_flows = dhmin.get_entities(model, ['Pin', 'Pot'])
     power_flows_grouped = power_flows.groupby(level='timesteps')
-    
+
     power_input = dhmin.get_entity(model, 'Q')
     power_input_grouped = power_input.groupby(level='timesteps')
-    
+
     plt.figure()
     for i, (name, group) in enumerate(power_flows_grouped):
         plt.subplot(2,3,i+1)
@@ -34,9 +34,9 @@ def plot_flows_min(model):
             z = 2 if value > 1 else 1 # show grey lines behind red ones
             w = max(0.5, value/80)
             colour = '#ff5500' if value > 1 else '#cccccc'
-            plt.plot(x, y, linewidth=w, color=colour, zorder=z, solid_capstyle='round') 
+            plt.plot(x, y, linewidth=w, color=colour, zorder=z, solid_capstyle='round')
         #group['Pin'].plot()
-        
+
         Q = power_input_grouped.get_group(name)['Q']
         for key, value in Q.iteritems():
             x = int(key[0]/10)
@@ -46,8 +46,8 @@ def plot_flows_min(model):
             colour = '#990000' if value > 1 else '#ffffff'
             plt.scatter(x, y, zorder=z, s=w, facecolors=colour, edgecolors='none', linewidths=0)
     plt.show()
-    
-    
+
+
 def symmetrize(df):
     """ Make a directed quantity (like y, Pin, Pot) symmetric (like Pmax or x).
     
